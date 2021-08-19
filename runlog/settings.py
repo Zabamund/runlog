56201f1831625ad31ae148b57542befee3eef1e1
Zabamund <fracgeol@gmail.com>
2021-08-19 10:19:57 +0200

    update settings for deploy

--- runlog/settings.py
+++ runlog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'zabamund.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Need to add static folder for webapp
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/imgs/'
 
